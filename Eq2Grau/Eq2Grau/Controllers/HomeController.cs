e23817d681f710d2529adebe554cd3fb283f95b4
andredrtsaipt <aluno21296@ipt.pt>
2023-03-23 13:06:36 +0000

    Cálculo das raízes

--- Eq2Grau/Eq2Grau/Controllers/HomeController.cs
+++ Eq2Grau/Eq2Grau/Controllers/HomeController.cs
@@ -10,7 +10,33 @@ namespace Eq2Grau.Controllers {
             _logger = logger;
         }
 
-        public IActionResult Index() {
+        public IActionResult Index(int A, int B, int C) {
+            // Variáveis auxiliares
+            double delta = B * B - 4 * A * C;
+            string x1 = "", x2 = "";
+
+            // Posso calcular as raízes?
+            if (A != 0) {
+                // Há raizes reais?
+                if (delta >= 0) {
+                    // Sim, há
+                    // x1 = (-b + sqrt(b^2 - 4ac)) / (2a)
+                    // x2 = (-b - sqrt(b^2 - 4ac)) / (2a)
+                    x1 = (-B + Math.Sqrt(delta)) / (2 * A) + "";
+                    x2 = (-B - Math.Sqrt(delta)) / (2 * A) + "";
+                } else {
+                    // Não há raízes reais
+                    // x1 = -b / (2a) + sqrt(-delta) / (2a) i
+                    // x2 = -b / (2a) - sqrt(-delta) / (2a) i
+                    x1 = -B / (2 * A) + " + " + Math.Sqrt(-delta) / (2 * A) + "i";
+                    x2 = -B / (2 * A) + " - " + Math.Sqrt(-delta) / (2 * A) + "i";
+                }
+            }
+
+            // Preparar os dados para serem enviados para a View
+            ViewBag.X1 = x1;
+            ViewBag.X2 = x2;
+
             return View();
         }
 
